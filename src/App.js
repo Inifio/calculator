8da4c25b1342b69ebf9b539b7dc0d80822b0be7a
Lawson Zeugin <lawson@inif.io>
2019-03-29 15:52:27 -0500

    Project Init
    What's Done:
    - React Boilerplate
    - UI
    - Number input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      beforeOperand: 0,
+      afterOperand: 0,
+      displayValue: "0",
+      secondValue: false,
+      isAddition: false,
+      isSubtraction: false,
+      isMultiplication: false,
+      isDivision: false,
+      isDecimal: false,
+    }
+  }
+
+  handleKeypress = (e, val) => {
+    let displayValueTemp = this.state.displayValue.slice();
+
+    // Input checks
+    if (val == 0 && displayValueTemp == 0)
+      return null;
+    
+    if (val == "." && !this.state.isDecimal) {
+      this.setState({ isDecimal: true })
+    } else if(val == "." && this.state.isDecimal) {
+      return null;
+    }
+
+    displayValueTemp == 0 ? displayValueTemp = val.toString() : displayValueTemp = displayValueTemp + val;
+
+    // Set States
+    this.setState({ displayValue: displayValueTemp });
+  }
+
+  clear = () => {
+    this.setState({ displayValue: "0" })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input ckassName="Display" value={this.state.displayValue}></input>
+
+        <div className="Keypad">
+          <div> <button className="options" onClick={this.clear}>CL</button> <button className="options">+/-</button> <button className="options">%</button> <button className="operand">/</button> </div>
+          <div> <button className="number" onClick={(e) => this.handleKeypress(e, 7)}>7</button> <button className="number" onClick={(e) => this.handleKeypress(e, 8)}>8</button> <button className="number" onClick={(e) => this.handleKeypress(e, 9)}>9</button> <button className="operand">X</button> </div>
+          <div> <button className="number" onClick={(e) => this.handleKeypress(e, 4)}>4</button> <button className="number" onClick={(e) => this.handleKeypress(e, 5)}>5</button> <button className="number" onClick={(e) => this.handleKeypress(e, 6)}>6</button> <button className="operand">-</button> </div>
+          <div> <button className="number" onClick={(e) => this.handleKeypress(e, 1)}>1</button> <button className="number" onClick={(e) => this.handleKeypress(e, 2)}>2</button> <button className="number" onClick={(e) => this.handleKeypress(e, 3)}>3</button> <button className="operand">+</button> </div>
+          <div> <button className="long-button number" onClick={(e) => this.handleKeypress(e, 0)}>0</button> <button className="number" onClick={(e) => this.handleKeypress(e, ".")}>.</button> <button className="operand">=</button> </div>
+        </div>
       </div>
     );
   }
